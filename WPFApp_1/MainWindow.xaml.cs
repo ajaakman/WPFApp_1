028a787aaa2d981718468fb7bc684bfe85e7e404
Artur Jaakman <jaakmanartur@gmail.com>
2018-08-26 22:30:59 +0100

    Ink Canvas.

--- WPFApp_1/MainWindow.xaml.cs
+++ WPFApp_1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -40,6 +42,81 @@ namespace WPFApp_1
                 }
             }
 
+        }
+
+        private void DrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            var radiobutton = sender as RadioButton;
+            string radioBPressed = radiobutton.Content.ToString();
+            if(radioBPressed == "Draw")
+            {
+                this.DrawingCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            }
+            else if (radioBPressed == "Erase")
+            {
+                this.DrawingCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+            }
+            else if (radioBPressed == "Select")
+            {
+                this.DrawingCanvas.EditingMode = InkCanvasEditingMode.Select;
+            }
+        }
+
+        private void DrawPanel_KeyUp(object sender, KeyEventArgs e)
+        {
+            if((int)e.Key >= 35 && (int)e.Key <= 68)
+            {
+                switch((int)e.Key)
+                {
+                    case 35:
+                        strokeAttr.Width = 2;
+                        strokeAttr.Height = 2;
+                        break;
+                    case 36:
+                        strokeAttr.Width = 4;
+                        strokeAttr.Height = 4;
+                        break;
+                    case 37:
+                        strokeAttr.Width = 6;
+                        strokeAttr.Height = 6;
+                        break;
+                    case 38:
+                        strokeAttr.Width = 8;
+                        strokeAttr.Height = 8;
+                        break;
+                    case 45:
+                        strokeAttr.Color = (Color)ColorConverter.ConvertFromString("Blue");
+                        break;
+                    case 50:
+                        strokeAttr.Color = (Color)ColorConverter.ConvertFromString("Green");
+                        break;
+                    case 68:
+                        strokeAttr.Color = (Color)ColorConverter.ConvertFromString("Yellow");
+                        break;
+                }
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("MyPicture.bin", FileMode.Create))
+            {
+                this.DrawingCanvas.Strokes.Save(fs);
+            }
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("MyPicture.bin", FileMode.Open, FileAccess.Read))
+            {
+                StrokeCollection sc = new StrokeCollection(fs);
+                this.DrawingCanvas.Strokes = sc;
+            }
         }
     }
 }
